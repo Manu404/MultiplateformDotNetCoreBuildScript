ac99dad8f3df8b6983d3cc014a2e1c4c0455f552
Manu404 <contact@emmanuelistace.be>
2023-04-13 08:58:40 +0200

    add support for portable or multifile compilation

--- multiplateform_build.sh
+++ multiplateform_build.sh
@@ -1,20 +1,39 @@
 #!/bin/bash
 
-echo -----------------------------------
-echo Settings up build
-echo -----------------------------------
+#https://stackoverflow.com/questions/5947742/how-to-change-the-output-color-of-echo-in-linux
+Green=$'\033[0;32m'        # Green
+Yellow=$'\033[0;33m'       # Yellow
+Color_Off=$'\033[0m'	   # Reset color
+Purple=$'\033[0;35m'       # Purple
+Cyan=$'\033[0;36m'         # Cyan
+BPurple=$'\033[1;35m'      # Purple
+BCyan=$'\033[1;36m'        # Cyan
+
+headerColor=$Purple
+subheaderColot=$Cyan
+BheaderColor=$BPurple
+BsubheaderColot=$BCyan
+
+echo ""
+echo "$headerColor -----------------------------------"
+echo "$BheaderColor Settings up build"
+echo "$headerColor -----------------------------------"
+echo ""
+echo "$Color_Off"
 
 # Default variables
 currentDir="$PWD"
 outputFolder="$currentDir/output/"
 selectedPlateform=""
 zipOutput="$outputFolder/zip/"
-project="";
+projectName="";
+projectVersion="";
+embeded=false
 
 #
 # PARSE COMMAND PARAMETER
 #
-VALID_ARGS=$(getopt -o p:t: --long plateform: -- "$@")
+VALID_ARGS=$(getopt -l target:projet:name:version: -o ep:t:n:v:  -- "$@")
 if [[ $? -ne 0 ]]; then
     exit 1;
 fi
@@ -22,6 +41,11 @@ fi
 eval set -- "$VALID_ARGS"
 while [ : ]; do
   case "$1" in
+	-e)
+		embeded=true
+        echo "Produce single file assembly"
+        shift 1
+        ;;
     -t | --target)
 		selectedPlateform="$2"
         echo "Provided plateform is $2"
@@ -30,6 +54,16 @@ while [ : ]; do
 	-p | --project)
 		project="$2"
         echo "Provided project is $2"
+        shift 2
+        ;;
+	-n | --name)
+		projectName="$2"
+        echo "Provided projet name is $2"
+        shift 2
+        ;;
+	-v | --version)
+		projectVersion="$2"
+        echo "Provided verison is $2"
         shift 2
         ;;
     --) shift; 
@@ -72,21 +106,63 @@ fi
 #
 # BUILD OF THE PROJECT
 #
-echo '-----------------------------------'
-echo 'Building project'
-echo '-----------------------------------'
-buildFolder="$outputFolder/build/$selectedPlateform/"
+echo ""
+echo "$subheaderColot -----------------------------------"
+echo "$BsubheaderColot Building project for $selectedPlateform"
+echo "$subheaderColot -----------------------------------"
+echo ""
+echo "$Color_Off"
 
-dotnet publish $project -c Release -r $selectedPlateform -p:PublishSingleFile=True --self-contained True -o $buildFolder
+if $embeded
+then
+	echo "embed"
+	buildFolder="$outputFolder/build/$selectedPlateform""_portable/"
+else
+	echo "normal"
+	buildFolder="$outputFolder/build/$selectedPlateform/"
+fi
+
+#rm-rf $project"/obj"
+dotnet clean $project -r $selectedPlateform
+dotnet publish $project -c Release -r $selectedPlateform -p:PublishSingleFile=$embeded --self-contained True -o $buildFolder
 
 
 #
 # PACKING OF THE BUILD OUTPUT
 #
-echo '-----------------------------------'
-echo 'Packing project'
-echo '-----------------------------------'
-zipDestination="$zipOutput/$selectedPlateform.zip"
+echo ""
+echo "$subheaderColot -----------------------------------"
+echo "$BsubheaderColot Packing project"
+echo "$subheaderColot -----------------------------------"
+echo ""
+echo "$Color_Off"
+
+echo "Project Name: '$projectName'"
+echo "Project Version: '$projectVersion'"
+echo "PlateformName: $selectedPlateform"
+
+zipDestination="$zipOutput/$projectName"
+
+if [ ! -z projectVersion ]
+then
+	zipDestination+="_"
+fi
+zipDestination+=$projectVersion;
+
+if [ ! -z projectVersion ] || [ ! -z projectName ]
+then
+	zipDestination+="_"
+fi
+zipDestination+=$selectedPlateform;
+
+if $embeded
+then
+	zipDestination+="_portable"
+fi
+
+zipDestination+=".zip"
+
+echo "Desitnation zip: $zipDestination"
 cd $buildFolder
 mkdir -p $zipOutput
 zip -r $zipDestination ./*
